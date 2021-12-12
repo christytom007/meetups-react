75f2cc0e9df3e823a713f9c9e94a8b2fb9aef3c5
Christy Thomas <christytom.007@gmail.com>
2021-12-12 16:13:23 -0500

    All Meetup Hooked with database All Meetup page update automatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://meetup-react-1eda2-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupsPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      navigate("/");
+    });
   }
 
   return (
