dde02e370f19cb9e7b59c7064ae7d86acf1d0e66
Christy Thomas <christytom.007@gmail.com>
2021-12-12 17:08:57 -0500

    Implemented Favorites Page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import FavoriteContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoriteCtx = useContext(FavoriteContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function MainNavigation() {
             <Link to="/new-meetups">Add New Meetups</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoriteCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
