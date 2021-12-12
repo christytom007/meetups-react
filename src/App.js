0086b8e5ce3040799dd5d711c059f5470253dcab
Christy Thomas <christytom.007@gmail.com>
2021-12-12 13:15:46 -0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetups" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
